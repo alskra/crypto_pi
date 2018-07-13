baecd4c0e2931f5b2be8259a055e2d49c2766d57
Alexandr <krasnoyurchenko.a.s@gmail.com>
2018-07-13 06:35:55 +0300

    upd

--- src/js/blocks/header.js
+++ src/js/blocks/header.js
@@ -24,7 +24,7 @@ function onScroll() {
     vw = window.innerWidth;
 
   if (vw >= 900) {
-    if (pageYOffset > (vw >= 1230 ? header.dataset.size : header.dataset.sizeMd)) {
+    if (pageYOffset > (vw >= 1230 ? 300 : 0)) {
       header.classList.add('header--fixed');
     } else {
       header.classList.remove('header--fixed');

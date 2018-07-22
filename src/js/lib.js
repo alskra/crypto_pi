f2285a163df711bfb46214c21ee298780603797e
Alexandr <krasnoyurchenko.a.s@gmail.com>
2018-07-22 18:23:57 +0300

    upd

--- src/js/lib.js
+++ src/js/lib.js
@@ -3,7 +3,12 @@
 import './core-js';
 import cssVars from 'css-vars-ponyfill';
 import 'focus-visible';
+import wowjs from 'wowjs';
 import $ from 'jquery';
 
+const WOW = wowjs.WOW;
+
+console.log(wowjs);
 cssVars();
+new WOW().init();
 window.jQuery = window.$ = $;
